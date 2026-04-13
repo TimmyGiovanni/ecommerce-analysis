8430d49165741b7a4b9fa1e89f156ad0fa8aac46
Timmy Giovanni <timmy.oei16@gmail.com>
2026-04-13 20:00:56 +0700

    Add files via upload

---
+++ scripts/product_category_analysis/general.sql
@@ -0,0 +1,330 @@
+-- PRODUCT CATEGORY performance
+-- GENERAL
+-- avg price, discount
+SELECT
+	product_category,
+	ROUND(AVG(price),2) avg_price,
+	ROUND(avg(discount_rate),4) avg_discount
+FROM orders
+GROUP BY product_category
+HAVING product_category LIKE 'general';
+
+-- customer segment
+SELECT
+	product_category,
+	customer_segment,
+	COUNT(customer_segment) total_customer
+FROM orders
+GROUP BY product_category,customer_segment
+HAVING product_category LIKE 'general';
+
+-- revenue identification
+-- 2010: increase, 2011: increase
+SELECT
+	year,
+	product_category,
+	CASE
+		WHEN total_revenue > LAG(total_revenue) OVER(PARTITION BY product_category ORDER BY year) THEN 'Increase'
+		WHEN total_revenue < LAG(total_revenue) OVER(PARTITION BY product_category ORDER BY year) THEN 'Decrease'
+		ELSE 'Same'
+	END performance
+FROM(
+SELECT
+	year,
+	product_category,
+	ROUND(SUM(sales_revenue),2) total_revenue
+FROM orders
+GROUP BY year, product_category
+)t
+WHERE product_category LIKE 'general'
+ORDER BY year, product_category;
+
+-- orders identification
+SELECT
+	year,
+	product_category,
+	CASE
+		WHEN amount > LAG(amount) OVER(PARTITION BY product_category ORDER BY year) THEN 'Increase'
+		WHEN amount < LAG(amount) OVER(PARTITION BY product_category ORDER BY year) THEN 'Decrease'
+		ELSE 'Same'
+	END performance
+FROM(
+SELECT
+	year,
+	product_category,
+	COUNT(product_category) amount
+FROM orders
+GROUP BY year, product_category
+)t
+WHERE product_category LIKE 'general'
+ORDER BY year, product_category;
+
+-- total orders: 1477
+SELECT
+	product_category,
+	count(*) total_orders
+FROM orders
+GROUP BY product_category
+HAVING product_category LIKE 'general';
+
+-- order amount
+-- 2009: 51, 2010: 688, 2011: 738
+SELECT
+	year,
+	product_category,
+	COUNT(product_category) amount
+FROM orders
+GROUP BY year, product_category
+HAVING product_category LIKE 'general'
+ORDER BY year, amount DESC;
+
+-- total revenue : 239,610.34
+SELECT
+	product_category,
+	SUM(sales_revenue) total_revenue
+FROM orders
+GROUP BY product_category
+HAVING product_category LIKE 'general';
+
+-- revenue
+-- 2009: 7,911.65, 2010: 110,731.68, 2011: 120,967.01
+SELECT
+	year,
+	product_category,
+	SUM(sales_revenue) revenue
+FROM orders
+GROUP BY year, product_category
+HAVING product_category LIKE 'general'
+ORDER BY year, revenue DESC;
+
+-- avg impression, reach, and ctr
+SELECT
+	product_category,
+	AVG(impressions) avg_impression,
+	AVG(market_reach) avg_reach,
+	ROUND(AVG(click_through_rate),4) avg_ctr
+FROM orders
+GROUP BY product_category
+HAVING product_category LIKE 'general';
+
+-- yearly avg impression, reach, and ctr
+SELECT
+	year,
+	product_category,
+	AVG(impressions) avg_impression,
+	AVG(market_reach) avg_reach,
+	ROUND(AVG(click_through_rate),4) avg_ctr
+FROM orders
+GROUP BY year, product_category
+HAVING product_category LIKE 'general'
+ORDER BY year;
+
+-- AOV: 162.23
+SELECT
+	product_category,
+	SUM(sales_revenue) / COUNT(product_category) as AOV
+FROM orders
+GROUP BY product_category
+HAVING product_category LIKE 'general';
+
+-- yearly AOV
+-- 2009: 163.91, 2010: 160.95, 2011: 155.13
+SELECT
+	year,
+	product_category,
+	ROUND(SUM(sales_revenue) / COUNT(product_category),2) as AOV
+FROM orders
+GROUP BY year, product_category
+HAVING product_category LIKE 'general';
+
+-- ad spend
+-- 2009: 28,098.45, 2010: 383,696.73, 2011: 3522,497.98
+SELECT 
+	year,
+	product_category,
+	ROUND(SUM(ad_spend),2) ad_spend
+FROM orders
+GROUP BY year,product_category
+HAVING product_category LIKE 'general'
+ORDER BY year;
+
+-- ctr
+-- 2009: 5.39%, 2010: 5.48%, 2011: 5.5%
+SELECT
+	product_category,
+	year,
+	ROUND(AVG(click_through_rate),4) avg_ctr
+FROM orders
+GROUP BY product_category, year
+HAVING product_category LIKE 'general'
+ORDER BY year;
+
+-- most channel used to purchase product
+-- 2009: Email 14, 2010: SM 193, 2011: SM 203
+SELECT
+	year,
+	product_category,
+	channel,
+	COUNT(*) channel_used
+FROM orders
+GROUP BY year, product_category, channel
+HAVING product_category LIKE 'general'
+ORDER BY year;
+
+-- generality index
+-- 2010: between -0.37 and 0.37, 2011: -0.37 and 0.37
+SELECT
+	year,
+	product_category,
+	seasonality_index,
+	COUNT(*) season_impact
+FROM orders
+GROUP BY year, product_category, seasonality_index
+HAVING product_category LIKE 'general'
+ORDER BY year;
+
+-- average campaign duration
+-- 2009: 38, 2010: 32, 2011: 31
+SELECT
+	year,
+	product_category,
+	AVG(campaign_duration_days) avg_campaign_duration
+FROM orders
+GROUP BY year, product_category
+HAVING product_category LIKE 'general'
+ORDER BY year;
+
+-- average customer lifetime value
+-- increasing over the years
+SELECT
+	year,
+	product_category,
+	ROUND(AVG(customer_lifetime_value),2) avg_clv
+FROM orders
+GROUP BY year, product_category
+HAVING product_category LIKE 'general'
+ORDER BY year;
+
+-- overall performance of the course of 2009-2011 in ads, ctr, and revenue
+-- high (above average), low (below average)
+-- low ads, high ctr, high reach, high revenue: 125 
+-- low ads, high ctr, low reach, high revenue: 168; could be buying in bulk, right targeting
+-- low ads, high ctr, ,low reach, low revenue: 91 ; not enough ads to attract and/or reach customers
+-- low ads, low ctr, low reach, high revenue: 201
+-- low ads, low ctr, low reach, low revenue: 200
+-- high ads, high ctr, high reach, high revenue: 8 
+WITH CTE_avg AS(
+SELECT
+	product_category,
+	ROUND(AVG(ad_spend),2) avg_ad_spend,
+	ROUND(AVG(market_reach),2) avg_reach,
+	ROUND(AVG(click_through_rate),4) avg_ctr,
+	ROUND(AVG(sales_revenue),2) avg_revenue
+FROM orders
+GROUP BY product_category
+)
+, cte_performance AS (
+		SELECT
+			year,
+			o.product_category,
+			CASE
+				WHEN ad_spend > avg_ad_spend THEN 'High'
+				WHEN ad_spend < avg_ad_spend THEN 'Low'
+				ELSE 'Average'
+			END ad_performance,
+			CASE
+				WHEN market_reach > avg_reach THEN 'High'
+				WHEN market_reach < avg_reach THEN 'Low'
+				ELSE 'Average'
+			END reach_performance,
+			CASE
+				WHEN click_through_rate > avg_ctr THEN 'High'
+				WHEN click_through_rate < avg_ctr THEN 'Low'
+				ELSE 'Average'
+			END ctr_performance,
+			CASE
+				WHEN sales_revenue > avg_revenue THEN 'High'
+				WHEN market_reach < avg_revenue THEN 'Low'
+				ELSE 'Average'
+			END revenue_performance
+		FROM orders o
+		LEFT JOIN CTE_avg ca
+		ON o.product_category = ca.product_category
+		)
+SELECT
+	COUNT(*)
+FROM cte_performance
+WHERE product_category LIKE 'general' AND revenue_performance LIKE 'high' AND ctr_performance LIKE 'high' AND ad_performance LIKE 'high' AND reach_performance LIKE 'high'
+
+-- ROAS
+-- overall ROAS: 3.92%
+SELECT
+	product_category,
+	ROUND(SUM(sales_revenue) / SUM(ad_spend),2) roas
+FROM orders
+GROUP BY product_category
+HAVING product_category LIKE 'general';
+
+-- yearly ROAS
+-- 2009: 3.66, 2010: 3.72, 2011: 4.13
+SELECT
+	year,
+	product_category,
+	ROUND(SUM(sales_revenue) / SUM(ad_spend),2) roas
+FROM orders
+GROUP BY year, product_category
+HAVING product_category LIKE 'general'
+ORDER BY year;
+
+-- ROAS by region
+-- east: 4.24, north: 4.03, west: 3.46, central: 0.93, south: 0.79
+SELECT 
+	region,
+	product_category,
+	ROUND(SUM(sales_revenue) / SUM(ad_spend),2) roas
+FROM orders
+GROUP BY region,product_category
+HAVING product_category LIKE 'general'
+ORDER BY roas DESC;
+
+-- yearly ROAS by region
+-- 2009: east: 19.46, 2010: central 10.83, 2011: north 4.46
+SELECT 
+	year,
+	region,
+	product_category,
+	ROUND(SUM(sales_revenue) / SUM(ad_spend),2) roas
+FROM orders
+GROUP BY year, region,product_category
+HAVING product_category LIKE 'general'
+ORDER BY year, roas DESC;
+
+-- AOV by region
+SELECT
+	region,
+	product_category,
+	ROUND(SUM(sales_revenue) / COUNT(product_category),2) AOV
+FROM orders
+GROUP BY region, product_category
+HAVING product_category LIKE 'general'
+ORDER BY AOV DESC;
+
+-- yearly AOV by region
+SELECT
+	year,
+	region,
+	product_category,
+	ROUND(SUM(sales_revenue) / COUNT(product_category),2) AOV
+FROM orders
+GROUP BY year, region, product_category
+HAVING product_category LIKE 'general'
+ORDER BY year, AOV DESC;
+
+-- avg competition index
+SELECT
+	product_category,
+	ROUND(AVG(competition_index),2) avg_competition_index
+FROM orders
+GROUP BY product_category
+HAVING product_category LIKE 'general';
+/*============================================================================================================*/
